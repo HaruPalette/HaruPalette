44b1f8d05e5bfcb0a32d973994b41d1ec7a9b3de
서예지 <syg9272@naver.com>
2023-03-16 18:00:02 +0900

    ✨ add login

--- fe/pages/_document.tsx
+++ fe/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* 카카오 SDK import */}
+        <script src="https://developers.kakao.com/sdk/js/kakao.min.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
