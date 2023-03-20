318aab6a860ce403a0871fb4d1edcf4519c224d6
임성빈 <mae03087@naver.com>
2023-03-20 16:12:30 +0900

    🎨 create page

---
+++ fe/pages/create/index.tsx
@@ -0,0 +1,13 @@
+import HomeButton from '../../components/button/HomeButton';
+import WeatherButton from '../../components/button/WeatherButton';
+
+function Create() {
+  return (
+    <>
+      <HomeButton></HomeButton>
+      <WeatherButton></WeatherButton>
+    </>
+  );
+}
+
+export default Create;
