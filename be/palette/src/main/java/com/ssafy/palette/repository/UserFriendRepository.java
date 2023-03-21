455b48ee890c2902b9bde44ee3ab77c8a6cd9b28
jiyeonyou0416 <jiyoenyou0416@naver.com>
2023-03-21 17:46:47 +0900

    :sparkles: add get profile

---
+++ be/palette/src/main/java/com/ssafy/palette/repository/UserFriendRepository.java
@@ -0,0 +1,17 @@
+package com.ssafy.palette.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.palette.domain.entity.Friend;
+import com.ssafy.palette.domain.entity.User;
+import com.ssafy.palette.domain.entity.UserFriend;
+
+public interface UserFriendRepository extends JpaRepository<UserFriend, Long> {
+
+	Optional<Friend> findFriendByUser(User user);
+
+	List<Object> findByUser(User user);
+}
