0a538107534caa657ba0ef05f76678863d8226bc
임성빈 <mae03087@naver.com>
2023-03-22 13:52:22 +0900

    🎨 weather

---
+++ fe/constants/weather.ts
@@ -0,0 +1,8 @@
+import { WeatherList } from '../types/commonTypes';
+
+export const WEATHER_LIST: WeatherList = {
+  Clear: 'vercel.svg',
+  Clouds: 'vercel.svg',
+  Rain: 'vercel.svg',
+  Snow: 'vercel.svg',
+};
