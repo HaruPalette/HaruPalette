583857fd01aba621c7dd8b96f0515edc05dfff13
서예지 <syg9272@naver.com>
2023-03-22 16:48:18 +0900

    🎨 create date hook

---
+++ fe/hooks/useDate.ts
@@ -0,0 +1,9 @@
+export function useDate() {
+  const newDate = new Date();
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const date = newDate.getDate();
+  const day = newDate.getDay();
+
+  return { year, month, date, day };
+}
