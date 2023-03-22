24e48988a168246adbf96983831f7ee05ffb8842
임성빈 <mae03087@naver.com>
2023-03-22 09:14:14 +0900

    🎨 diaries service

---
+++ fe/services/DiariesService.ts
@@ -0,0 +1,31 @@
+import { DIARIES } from '../constants/api';
+import { axiosInstance } from '../utils/axios';
+
+class DiariesService {
+  /** 일기 상세 조회 */
+  public static async getDiaries(diaryId: number) {
+    const response = await axiosInstance.get(DIARIES, {
+      params: { diaryId: diaryId },
+    });
+
+    return response;
+  }
+
+  /** 일기 작성 */
+  public static async postDiaries(params: object) {
+    const response = await axiosInstance.post(DIARIES, {
+      params,
+    });
+
+    return response;
+  }
+
+  /** 일기 삭제 */
+  public static async patchDiaries(diaryId: number) {
+    const response = await axiosInstance.patch(DIARIES, {
+      params: { diaryId: diaryId },
+    });
+  }
+}
+
+export default DiariesService;
