7ab04ee48b6a20941d21b2cd07f895108ca6353d
임성빈 <mae03087@naver.com>
2023-03-23 09:14:42 +0900

    🎨 main index

--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -3,14 +3,23 @@ import { Inter } from 'next/font/google';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import ScrollToTopButton from '../components/button/ScrollToTopButton';
+import Ball from '../components/animation/Ball';
 
 // const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const BALL_DATA = {
+    top: '100px',
+    left: '100px',
+    width: 500,
+    height: 500,
+    color: 'black',
+  };
+
   return (
     <>
       <Header />
-
+      <Ball ballData={BALL_DATA}></Ball>
       <ScrollToTopButton></ScrollToTopButton>
       <Footer />
     </>
