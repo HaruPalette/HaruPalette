c4b1a0b3a3830a929af5ae4d261c6fa038aa82ea
임성빈 <mae03087@naver.com>
2023-03-23 11:29:46 +0900

    🎨 button constants

---
+++ fe/constants/button.ts
@@ -0,0 +1,33 @@
+import { ButtonData } from '../types/commonTypes';
+
+export const CHOOSE_BUTTON: ButtonData = {
+  width: 3,
+  height: 2,
+  context: '선택하기',
+  fs: 1,
+  image: '',
+};
+
+export const TALK_BUTTON: ButtonData = {
+  width: 10,
+  height: 3,
+  context: '대화하기',
+  fs: 1.5,
+  image: '',
+};
+
+export const STOP_BUTTON: ButtonData = {
+  width: 5,
+  height: 3,
+  context: '종료하기',
+  fs: 2,
+  image: '',
+};
+
+export const WRITE_BUTTON: ButtonData = {
+  width: 10,
+  height: 4,
+  context: '일기작성하러가기',
+  fs: 3,
+  image: '',
+};
