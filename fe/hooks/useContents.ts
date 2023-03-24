e313417b865da9c750a5a3b7a40046347fc4c402
서예지 <syg9272@naver.com>
2023-03-24 16:43:41 +0900

    🎨 update contents hook

---
+++ fe/hooks/useContents.ts
@@ -0,0 +1,24 @@
+export function useContents(contents: string) {
+  const arr: string[] = contents.split(' ');
+  let contentList = [];
+  let temp = [];
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (count + arr[i].length <= 25) {
+      temp.push(arr[i]);
+      count += arr[i].length;
+    } else {
+      contentList.push(temp.join(' '));
+      temp = [];
+      count = 0;
+      i--;
+    }
+  }
+  if (temp) contentList.push(temp.join(' '));
+
+  return contentList;
+}
+
+export function useAnswer(answer: string) {
+  return answer.split('\n');
+}
