ea2ed54caaa56a256a7509a091a32dd34f53856d
syg9272 <syg9272@naver.com>
2023-03-27 00:30:45 +0900

    🎨 add save image

--- fe/components/button/SaveImageButton.tsx
+++ fe/components/button/SaveImageButton.tsx
@@ -1,11 +1,19 @@
+import { ColorTypes } from '@emotion/react';
 import styled from '@emotion/styled';
 import Image from 'next/image';
+import { Dispatch, SetStateAction } from 'react';
+import useTheme from '../../hooks/useTheme';
+import { common } from '../../styles/theme';
 
-function SaveImageButton() {
+function SaveImageButton(props: {
+  setSave: Dispatch<SetStateAction<boolean>>;
+}) {
+  const { setSave } = props;
+  const theme = useTheme();
   return (
-    <SaveButton type="button">
-      <Image
-        src="assets/img/common/save.svg"
+    <SaveButton type="button" theme={theme} onClick={() => setSave(true)}>
+      <SaveImg
+        src="/assets/img/common/save.svg"
         width={40}
         height={40}
         alt="share"
@@ -17,4 +25,20 @@ function SaveImageButton() {
 
 export default SaveImageButton;
 
-const SaveButton = styled.button``;
+const SaveButton = styled.button<{ theme: ColorTypes }>`
+  width: 20rem;
+  height: 3.5rem;
+  border-radius: 3.5rem;
+  font-weight: ${common.fontSize.fs20};
+  font-weight: bold;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid ${props => props.theme.border};
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25);
+`;
+
+const SaveImg = styled(Image)`
+  margin-right: 1rem;
+`;
