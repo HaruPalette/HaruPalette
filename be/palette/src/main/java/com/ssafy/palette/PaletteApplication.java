2a92aa15970f800c5a4aeb16c57fe729a7447d9b
유지연 <jiyoenyou0416@naver.com>
2023-03-28 11:11:04 +0900

    :busts_in_silhouette: update timezone setting

--- be/palette/src/main/java/com/ssafy/palette/PaletteApplication.java
+++ be/palette/src/main/java/com/ssafy/palette/PaletteApplication.java
@@ -1,5 +1,9 @@
 package com.ssafy.palette;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +11,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class PaletteApplication {
+
+	@PostConstruct
+	public void started() {
+		// timezone KST 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaletteApplication.class, args);
--- be/palette/src/main/java/com/ssafy/palette/service/PointService.java
+++ be/palette/src/main/java/com/ssafy/palette/service/PointService.java
@@ -1,9 +1,13 @@
 package com.ssafy.palette.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.palette.domain.entity.Point;
 import com.ssafy.palette.domain.entity.User;
+import com.ssafy.palette.repository.PointRepository;
 import com.ssafy.palette.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,11 +18,26 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 @RequiredArgsConstructor
 public class PointService {
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
+	private final PointRepository pointRepository;
 
-    public void earnPoint(String userId, int val) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
-        user.setPoint(user.getPoint()+val);
-    }
+	public void earnPoint(String userId, int val) {
+		User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
+		user.setPoint(user.getPoint() + val);
+	}
 
+	public void usePoint(String userId, int val) {
+		User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
+		user.setPoint(user.getPoint() - val);
+	}
+
+	public void addHistory(String useId, int val, String category, LocalDateTime date) {
+		Point point = Point.builder()
+			.user(userRepository.findById(useId).get())
+			.point(val)
+			.category(category)
+			.date(date)
+			.build();
+		pointRepository.save(point);
+	}
 }
