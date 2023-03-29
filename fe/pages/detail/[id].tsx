516b3fcdb21e38aad244ced0e74f6f731e4aa236
서예지 <syg9272@naver.com>
2023-03-29 13:44:19 +0900

    🎨 add emotion bar

--- fe/pages/detail/[id].tsx
+++ fe/pages/detail/[id].tsx
@@ -1,13 +1,16 @@
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { ColorTypes } from '@emotion/react';
 import { DiaryData } from '../../types/diariesTypes';
 import Diary from '../../components/diary/Diary';
-import KakaoShareButton from '../../components/button/KakaoShareButton';
 import SaveImageButton from '../../components/button/SaveImageButton';
 import Header from '../../components/common/Header';
 import useTheme from '../../hooks/useTheme';
 import JellyList from '../../components/common/JellyList';
+import DeleteButton from '../../components/button/DeleteButton';
+import Horizontal from '../../components/progressbar/Horizontal';
+import { common } from '../../styles/theme';
 
 const DetailPage = styled.div<{ theme: ColorTypes }>`
   width: 100vw;
@@ -22,8 +25,9 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
+  padding-bottom: 2rem;
 
   @media all and (max-width: 960px) {
     flex-direction: column;
@@ -32,18 +36,15 @@ const Container = styled.div`
     flex-direction: column;
   }
 `;
-
-const ButtonList = styled.div`
-  height: 40rem;
+const DetailList = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: center;
 
   transform: scale(1);
 
   @media all and (max-width: 960px) {
-    height: 10rem;
     margin-bottom: 2rem;
   }
 
@@ -51,6 +52,39 @@ const ButtonList = styled.div`
     transform: scale(0.65);
     margin-top: -150px;
     margin-bottom: 1rem;
+  }
+`;
+const EmotionList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Emotion = styled.div<{ color: string }>`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 2rem 0 0.5rem 0;
+  color: ${props => props.color};
+  font-size: ${common.fontSize.fs20};
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 1);
+`;
+
+const ButtonList = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 1.5rem;
+
+  transform: scale(1);
+
+  @media all and (max-width: 500px) {
+    transform: scale(0.65);
   }
 `;
 
@@ -65,7 +99,7 @@ function Detail() {
     diaryId: 1,
     date: '2023-03-12',
     contents:
-      '오늘은 팀 사진을 찍었다. 사진을 보았는데 정말 인간지네 같다. 잊지 못할 것 같은 하루다. 오늘은 팀 사진을 찍었다. 사진을 보았는데 정말 인간지네 같다. 잊지 못할 것 같은 하루다. 오늘은 팀 사진을 찍었다. 사진을 보았는데 정말 인간지네 같다. 잊지 못할 것 같은 하루다.',
+      '오늘은 팀 사진을 찍었다. 사진을 보았는데 정말 인간지네 같다. 잊지 못할 것 같은 하루다. 오늘은 팀 사진을 찍었다. 사진을 보았는데 정말 인간지네 같다. 잊지 못할 것 같은 하루다. 오늘은 팀 사진을 찍었다. 사진을 보았는데 정말 인간지네 같다. 잊지 못할 것 같은 하루다. ',
     weather: 'Clear',
     ename: 'haru',
     answer: `너는 좋은 일들만 끌어당겨
@@ -81,6 +115,50 @@ function Detail() {
     anxiety: 2,
     sadness: 2,
   };
+  const emotion = [
+    {
+      emotion: '행복',
+      icon: '/assets/img/common/happy.svg',
+      percent: diary.happy,
+      color: '#FFEE94',
+    },
+    {
+      emotion: '슬픔',
+      icon: '/assets/img/common/sadness.svg',
+      percent: diary.sadness,
+      color: '#ADDCFF',
+    },
+    {
+      emotion: '분노',
+      icon: '/assets/img/common/anger.svg',
+      percent: diary.anger,
+      color: '#FF9393',
+    },
+    {
+      emotion: '불안',
+      icon: '/assets/img/common/anxiety.svg',
+      percent: diary.anxiety,
+      color: '#ECC9A0',
+    },
+    {
+      emotion: '중립',
+      icon: '/assets/img/common/neutral.svg',
+      percent: diary.neutral,
+      color: '#DFDFDF',
+    },
+    {
+      emotion: '당황',
+      icon: '/assets/img/common/surprise.svg',
+      percent: diary.surprise,
+      color: '#DDC0E8',
+    },
+    {
+      emotion: '혐오',
+      icon: '/assets/img/common/disgust.svg',
+      percent: diary.disgust,
+      color: '#68B570',
+    },
+  ];
   useEffect(() => {
     //   이걸로 상세조회 요청 (일기 PK값)
     console.log(window.location.href.split('detail/')[1]);
@@ -98,10 +176,27 @@ function Detail() {
           setSave={setSave}
           setShare={setShare}
         />
-        <ButtonList>
-          <SaveImageButton setSave={setSave} />
-          <KakaoShareButton setShare={setShare} />
-        </ButtonList>
+        <DetailList>
+          <EmotionList>
+            {emotion.map(item => {
+              return (
+                <>
+                  <Emotion color={item.color}>
+                    <Image src={item.icon} width={40} height={40} alt="icon" />
+                    <div>
+                      {item.emotion} {item.percent}%
+                    </div>
+                  </Emotion>
+                  <Horizontal percent={item.percent} color={item.color} />
+                </>
+              );
+            })}
+          </EmotionList>
+          <ButtonList>
+            <SaveImageButton setSave={setSave} />
+            <DeleteButton />
+          </ButtonList>
+        </DetailList>
       </Container>
     </DetailPage>
   );
