31cc3ebed9ad76e82e41927e781e745eb9a86f4f
유지연 <jiyoenyou0416@naver.com>
2023-03-30 14:24:33 +0900

    :art: update SecurityConfig

--- be/palette/src/main/java/com/ssafy/palette/config/SecurityConfig.java
+++ be/palette/src/main/java/com/ssafy/palette/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.ssafy.palette.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+
+import com.ssafy.palette.filter.JwtAuthFilter;
+import com.ssafy.palette.provider.TokenProvider;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+	@Value("${jwt.secret}")
+	private String secret;
+
+	@Value("${jwt.token-validity-in-seconds}")
+	private String time;
 
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,9 +33,14 @@ public class SecurityConfig {
 			// REST 방식 사용 -> csrf, form로그인, 세션 무시
 			.httpBasic().disable()
 			.csrf().disable()
+			.addFilterBefore(new JwtAuthFilter(new TokenProvider(secret, Long.parseLong(time))),
+				UsernamePasswordAuthenticationFilter.class)
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			// 인가
+			.and()
 			.authorizeRequests()
-			.antMatchers("/api/**").permitAll()
+			.antMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
 			.anyRequest().authenticated();
 
 		return http.build();
