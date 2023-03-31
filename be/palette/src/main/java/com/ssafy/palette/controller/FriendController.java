0ab9a03393d8fe25e7494ef0f028612ef80332ae
유지연 <jiyoenyou0416@naver.com>
2023-03-31 13:26:47 +0900

    :art: update word & MySQL ip & auth ip

--- be/palette/src/main/java/com/ssafy/palette/controller/FriendController.java
+++ be/palette/src/main/java/com/ssafy/palette/controller/FriendController.java
@@ -39,7 +39,7 @@ public class FriendController {
 		String userId = jwtUtil.getUid(token);
 
 		FriendListDto friendListDto = friendService.getFriendList(userId);
-		return new ResponseEntity<>(friendListDto, HttpStatus.OK);
+		return new ResponseEntity<FriendListDto>(friendListDto, HttpStatus.OK);
 	}
 
 	// 캐릭터 선택
@@ -49,8 +49,8 @@ public class FriendController {
 		String token = header.get("Authorization").get(0).substring(7);   // 헤더의 토큰 파싱 (Bearer 제거)
 		String userId = jwtUtil.getUid(token);
 
-		friendService.chooseFriend(userId, friendId);
-		return new ResponseEntity<>(HttpStatus.OK);
+		int currentPoint = friendService.chooseFriend(userId, friendId);
+		return new ResponseEntity<Integer>(currentPoint, HttpStatus.OK);
 	}
 
 	// 캐릭터 구매
@@ -61,7 +61,7 @@ public class FriendController {
 		String token = header.get("Authorization").get(0).substring(7);   // 헤더의 토큰 파싱 (Bearer 제거)
 		String userId = jwtUtil.getUid(token);
 
-		friendService.makeFriend(userId, userFriendDto.getFriendId());
-		return new ResponseEntity<>(HttpStatus.OK);
+		int currentPoint = friendService.makeFriend(userId, userFriendDto.getFriendId());
+		return new ResponseEntity<Integer>(currentPoint, HttpStatus.OK);
 	}
 }
--- be/palette/src/main/java/com/ssafy/palette/service/ImageService.java
+++ be/palette/src/main/java/com/ssafy/palette/service/ImageService.java
@@ -15,15 +15,16 @@ import lombok.RequiredArgsConstructor;
 
 public class ImageService {
 	String defaultPath = "https://haru-palette.s3.ap-northeast-2.amazonaws.com/default/";
-	ArrayList<String> defaultImages =new ArrayList<>(List.of("castell.jpg"));
+	ArrayList<String> defaultImages = new ArrayList<>(
+		List.of("haru_light.svg", "haru_dark.svg", "gomi_light.svg", "gomi_light.svg", "tori_light.svg",
+			"tori_light.svg"));
 	Random rand;
 
-	public String getRandomDefaultImage()
-	{
+	public String getRandomDefaultImage() {
 		rand = new Random();
 		int index = rand.nextInt(defaultImages.size());
 
-		return defaultPath+defaultImages.get(index);
+		return defaultPath + defaultImages.get(index);
 	}
 
 }
