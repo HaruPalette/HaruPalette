c078f11b7c0dd56d0c510ac82652c7ac1bbecd24
LimSB-dev <mae03087@naver.com>
2023-04-02 19:43:12 +0900

    💫 add pulse in section3

--- fe/components/main/section3/Section3.tsx
+++ fe/components/main/section3/Section3.tsx
@@ -1,16 +1,20 @@
 import styled from '@emotion/styled';
 import useScreenY from '../../../hooks/useScreenY';
+import Pulse from '../../animation/Pulse';
 
-const Section = styled.section`
-  width: 100%;
+const Section = styled.section<{ windowHeight: number }>`
+  width: 100vw;
   height: 100vh;
-  display: flex;
+  display: ${props => (props.windowHeight >= 3900 ? 'flex' : 'none')};
   align-items: center;
   position: relative;
+
+  padding: 0 10rem;
 
   transition: font-size 0s ease-in-out;
 
   @media screen and (max-width: 960px) {
+    padding: 0 1rem;
   }
 
   @media screen and (max-width: 500px) {
@@ -23,8 +27,8 @@ function Section3() {
   const windowHeight = useScreenY();
 
   return (
-    <Section>
-      <article></article>
+    <Section windowHeight={windowHeight}>
+      <Pulse />
       <SectionText>언제, 어디서든</SectionText>
     </Section>
   );
