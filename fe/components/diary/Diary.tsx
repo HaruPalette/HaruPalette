307597ccbf9120a6bced61d2889877a1ebb7794c
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-04-06 00:49:08 +0900

    ⚡️ reset diary image

--- fe/components/diary/Diary.tsx
+++ fe/components/diary/Diary.tsx
@@ -18,7 +18,11 @@ import { useAppDispatch, useAppSelector } from '../../hooks/reduxHook';
 import { selectWeather } from '../../store/modules/weather';
 import { selectScript } from '../../store/modules/script';
 import { selectProfile } from '../../store/modules/profile';
-import { changeImageSuccess, selectDiary } from '../../store/modules/diary';
+import {
+  changeImageSuccess,
+  resetImageSuccess,
+  selectDiary,
+} from '../../store/modules/diary';
 
 const DetailStyles = styled.div<{ theme: ColorTypes }>`
   width: 30rem;
@@ -151,6 +155,16 @@ const InfoText = styled.h1`
   text-shadow: 0 0 4px rgba(255, 255, 255, 0.5);
 `;
 
+const CustomButton = styled.button<{ theme: ColorTypes }>`
+  position: fixed;
+  top: 21rem;
+  right: 3rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  color: ${props => props.theme.color};
+  cursor: pointer;
+`;
+
 const ChrSticker = styled(Image)`
   margin: auto;
 `;
@@ -210,6 +224,11 @@ function Diary(props: {
     const newfile = event.target.files?.[0];
     const newImage = URL.createObjectURL(newfile);
     dispatch(changeImageSuccess({ image: newImage, file: newfile }));
+  };
+
+  /** 이미지 초기화 */
+  const handleResetImage = () => {
+    dispatch(resetImageSuccess());
   };
 
   const weather = useAppSelector(selectWeather).curWeather;
@@ -283,11 +302,6 @@ function Diary(props: {
         <ImageContainer
           onDrop={handleDrop}
           onDragOver={event => event.preventDefault()}
-          onClick={() => {
-            if (type !== 'modify') {
-              window.open(image);
-            }
-          }}
         >
           {type === 'modify' ? (
             <>
@@ -307,11 +321,25 @@ function Diary(props: {
                   클릭
                 </InfoText>
                 <Dimmed />
-                <DiaryImage url={image === '' ? diary?.image : image} />
+                <DiaryImage url={image === '' ? diary.image : image} />
               </label>
+              <CustomButton
+                type="button"
+                onClick={handleResetImage}
+                theme={theme}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M105.1 202.6c7.7-21.8 20.2-42.3 37.8-59.8c62.5-62.5 163.8-62.5 226.3 0L386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H463.5c0 0 0 0 0 0h.4c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5zM39 289.3c-5 1.5-9.8 4.2-13.7 8.2c-4 4-6.7 8.8-8.1 14c-.3 1.2-.6 2.5-.8 3.8c-.3 1.7-.4 3.4-.4 5.1V448c0 17.7 14.3 32 32 32s32-14.3 32-32V396.9l17.6 17.5 0 0c87.5 87.4 229.3 87.4 316.7 0c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.5 62.5-163.8 62.5-226.3 0l-.1-.1L125.6 352H176c17.7 0 32-14.3 32-32s-14.3-32-32-32H48.4c-1.6 0-3.2 .1-4.8 .3s-3.1 .5-4.6 1z" />
+                </svg>
+              </CustomButton>
             </>
           ) : (
-            <DiaryImage url={diary?.image} />
+            <DiaryImage
+              url={diary?.image}
+              onClick={() => {
+                window.open(diary?.image);
+              }}
+            />
           )}
         </ImageContainer>
         <ContentList theme={theme} type={type}>
