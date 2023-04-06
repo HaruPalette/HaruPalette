b8769cd50392d9e122a66a39e95ce96f732fb641
길상욱 <sanguk9805@naver.com>
2023-04-06 16:53:14 +0900

    :spakles: bgm

--- fe/components/button/MusicButton.tsx
+++ fe/components/button/MusicButton.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import styled from '@emotion/styled';
-import { useAppSelector } from '../../hooks/reduxHook';
-import { selectProfile } from '../../store/modules/profile';
+import { useRef } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHook';
+import { selectProfile, setIsPlay } from '../../store/modules/profile';
 import { selectTheme } from '../../store/modules/theme';
 
 const MusicImage = styled(Image)`
   margin-right: 1rem;
+  cursor: pointer;
 
   @media all and (max-width: 500px) {
     width: 2rem;
@@ -17,15 +19,32 @@ const MusicImage = styled(Image)`
 function MusicButton() {
   const user = useAppSelector(selectProfile);
   const dark = useAppSelector(selectTheme);
+  const dispatch = useAppDispatch();
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const playHandler = () => {
+    if (user.isPlay) {
+      dispatch(setIsPlay(false));
+      audioRef.current?.pause();
+    } else {
+      dispatch(setIsPlay(true));
+      audioRef.current?.play();
+    }
+  };
   return (
-    <MusicImage
-      src={`/assets/img/${user.chrName}/${dark ? 'dark' : 'light'}/${
-        user.isPlay ? 'stop' : 'play'
-      }.svg`}
-      width={40}
-      height={40}
-      alt="play"
-    />
+    <>
+      <MusicImage
+        src={`/assets/img/${user.chrName}/${dark ? 'dark' : 'light'}/${
+          user.isPlay ? 'stop' : 'play'
+        }.svg`}
+        width={40}
+        height={40}
+        onClick={playHandler}
+        alt="play"
+      />
+      <audio autoPlay loop ref={audioRef}>
+        <source src="/assets/sound/bgm.mp3" type="audio/mpeg" />
+      </audio>
+    </>
   );
 }
 
